13b150fbdc06691ad26e43e5ba3899f9a4f99df1
siddhantparekh7 <48479314+siddhantparekh7@users.noreply.github.com>
2022-11-30 06:52:56 +0530

    commit_sid

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
+  const [user, setUser] = useState({});
   return (
     <div className='px-4 w-full h-screen flex justify-center items-center bg-login bg-no-repeat bg-cover'>
-      <form
-        action=''
+      <div
         className='border bg-white p-6 flex flex-col items-center min-w-[17rem] sm:min-w-[22rem] md:min-w-[35rem] max-w-[25rem]'
       >
         <h1 className='uppercase text-xl mb-4 font-bold'>Sign up</h1>
@@ -15,11 +16,21 @@ const Signup = () => {
             className='block p-2 border-2 rounded focus:outline-none'
             type='text'
             placeholder='First Name'
+            onChange={(event)=>{
+              let obj = user;
+              obj['firstName'] = event.target.value;
+              setUser(obj)
+            }}
           />
           <input
             className='block p-2 border-2 rounded focus:outline-none'
             type='text'
             placeholder='Last Name'
+            onChange={(event)=>{
+              let obj = user;
+              obj['lastName'] = event.target.value;
+              setUser(obj)
+            }}
           />
         </div>
         <div className='grid gap-4 md:grid-cols-2 mb-4'>
@@ -27,11 +38,21 @@ const Signup = () => {
             className='block p-2 border-2 rounded focus:outline-none'
             type='text'
             placeholder='Username'
+            onChange={(event)=>{
+              let obj = user;
+              obj['username'] = event.target.value;
+              setUser(obj)
+            }}
           />
           <input
             className='block p-2 border-2 rounded focus:outline-none'
             type='text'
             placeholder='Email'
+            onChange={(event)=>{
+              let obj = user;
+              obj['email'] = event.target.value;
+              setUser(obj)
+            }}
           />
         </div>
         <div className='grid gap-4 md:grid-cols-2 mb-4'>
@@ -39,11 +60,21 @@ const Signup = () => {
             className='block p-2 border-2 rounded focus:outline-none'
             type='password'
             placeholder='Password'
+            onChange={(event)=>{
+              let obj = user;
+              obj['password'] = event.target.value;
+              setUser(obj)
+            }}
           />
           <input
             className='block p-2 border-2 rounded focus:outline-none'
             type='password'
             placeholder='Confirm Password'
+            onChange={(event)=>{
+              let obj = user;
+              obj['cPassword'] = event.target.value;
+              setUser(obj)
+            }}
           />
         </div>
 
@@ -54,11 +85,18 @@ const Signup = () => {
             Privacy policy
           </a>
         </p>
-        <button className='mb-4 bg-teal-700 text-white p-2'>Create</button>
+        <button className='mb-4 bg-teal-700 text-white p-2' onClick={async ()=>{
+          console.log("user", user);
+          try{
+            let {data, status} = await axios.post('http://localhost:5000/api/auth/register', user);
+          }catch(err){
+            throw err;
+          }
+        }}>Create</button>
         <Link to='/login' className='capitalize underline mb-4'>
           Already have an account
         </Link>
-      </form>
+      </div>
     </div>
   );
 };
